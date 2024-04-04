2e9f68a7b75f72c36a7ec465605b0a2198de375d
ADRIEL <ADRIEL@DESKTOP-OP9TCVA>
2024-04-04 15:17:04 -0300

    Ejercicio 1 resuelto - Troncoso Adriel

---
+++ src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,18 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 5;
+		int num2 = 6;
+		int num3 = 3;
+		int num4 = 1;
+		int num5 = 4;
+		double prom = (double)(num1 + num2 + num3 + num4 + num5)/5;
+		
+		System.out.println(num1 + ", " +num2 + ", " +num3 + ", " +num4 + ", " +num5);
+		System.out.println("El promedio es: "+ prom);
+	}
+
+}

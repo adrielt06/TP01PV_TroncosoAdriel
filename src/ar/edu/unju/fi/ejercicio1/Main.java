67831cb05424e82945240fc48b65cab8c7affd64
ADRIEL <ADRIEL@DESKTOP-OP9TCVA>
2024-04-06 01:40:05 -0300

    Correccion Ejercicio 1 y 2 - Adriel Troncoso

--- src/ar/edu/unju/fi/ejercicio1/Main.java
+++ src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -4,13 +4,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// 5 variables de tipo entero que almacenan valores numéricos
 		int num1 = 5;
 		int num2 = 6;
 		int num3 = 3;
 		int num4 = 1;
 		int num5 = 4;
+		//Calculo de promedio
 		double prom = (double)(num1 + num2 + num3 + num4 + num5)/5;
-		
+		//Muestra de resultado por consola
 		System.out.println(num1 + ", " +num2 + ", " +num3 + ", " +num4 + ", " +num5);
 		System.out.println("El promedio es: "+ prom);
 	}
--- src/ar/edu/unju/fi/ejercicio2/Main.java
+++ src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -4,7 +4,23 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//Declaracion y asignacion de variables
+		String pais = "Argentina";
+		short edad = 22;
+		float hEdificio = (float)150.4;
+		float precio = (float)120.5;
+		long telefono = 380658925l;
+		double coseno = Math.cos(0.5);
+		
+		//Muestra de variables por consola
+		System.out.println("El nombre de un país: " + pais);
+		System.out.println("La edad de una persona: " + edad);
+		System.out.println("La altura de un edificio: " + hEdificio);
+		System.out.println("El precio de un producto de supermercado: " + precio);
+		System.out.println("Número de teléfono: " + telefono);
+		System.out.println("El cálculo de coseno de 0.5: "+ coseno);
+		
+		
 	}
 
 }
